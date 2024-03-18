8c1c7ba990da046f3b932e15d41f011b527a117f
stephen.adusah <stephen@walulel.com>
2024-03-18 10:31:05 +0000

    Added support for audio only download

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,20 +1,37 @@
 from pytube import YouTube
 
-try:
-    # Ask the user to input the YouTube URL
-    url = input("Enter the YouTube URL: ")
+def download_video(url):
+        try:
+            yt = YouTube(url)
+            
+            print("Title:", yt.title)
+            print("Views:", yt.views)
     
-    yt = YouTube(url)
-    
-    print("Title:", yt.title)
-    print("Views:", yt.views)
+            # Get the highest resolution stream
+            yd = yt.streams.get_highest_resolution()
+            
+            # Download the video to the current directory
+            yd.download()
+            
+            print("Download complete.")
+        except Exception as e:
+            print("An error occurred:", str(e))
 
-    # Get the highest resolution stream
-    yd = yt.streams.get_highest_resolution()
-    
-    # Download the video to the current directory
-    yd.download()
-    
-    print("Download complete.")
-except Exception as e:
-    print("An error occurred:", str(e))
+def download_audio(url):
+        try:
+            yt = YouTube(url)
+
+            # Get highest bitrate audio stream for given codec (defaults to mp4)
+            yd = yt.streams.get_audio_only()
+            
+            # Download the audio in .mp4 extention to the current directory
+            yd.download()
+            
+            print("Download complete.")
+        except Exception as e:
+            print("An error occurred:", str(e))
+
+# Ask the user to input the YouTube URL
+url = input("Enter the YouTube URL: ")
+download_video(url)
+  
